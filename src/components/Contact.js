48b02163ed4b92bac311d4d5b033c236f4a1bbf1
Amelie Macquet <amelie.macquet.am@gmail.com>
2020-10-13 12:04:08 +0200

    exos contact react

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import './Contact.css';
 
+const name = "Gloria Henry";
+const avatar = "https://randomuser.me/api/portraits/women/56.jpg " ;
+const onLine = true;  
+
+
 function Contact(){
     return(
         <div className="Contact">
-        <img className="avatar" src="https://randomuser.me/api/portraits/women/56.jpg"alt= "profil"/>
-        <div>
-           <div className="Gloria Henry"></div>
-          
+            <img className="avatar" src={avatar} alt= "profil"/>
+            <div>  
+               <div className="name">{name}</div>  
+               <div className="status">
+                  
+                  <p className="status-text">{onLine? <div className="status-online"></div>: <div className="status-offline"></div>}</p>
+                  <p className="status-text">{onLine? "online":"offline"}</p>
+            </div>
+
+             
         </div>
+          
+        
         
     
-      </div>
+        </div>
     )
 }
 export default Contact;
