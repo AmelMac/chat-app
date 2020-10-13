ae5dee0ae95b23779fc9820222719b91244ed5e1
Amelie Macquet <amelie.macquet.am@gmail.com>
2020-10-13 09:34:40 +0200

    components contact react

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact(){
+    return(
+        <div className="Contact">
+        <img className="avatar" src="https://randomuser.me/api/portraits/women/56.jpg"alt= "profil"/>
+        <div>
+           <div className="Gloria Henry"></div>
+          
+        </div>
+        
+    
+      </div>
+    )
+}
+export default Contact;
